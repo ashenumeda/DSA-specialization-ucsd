a619a1397510f5a0c518c01c6bcfbb6010924559
Ashen Umeda <ashenumeda@gmail.com>
2026-02-25 14:28:04 +0530

    Solve 01-algorithmic-toolbox week2 last_digit_of_fibonacci_number problem

--- 01-algorithmic-toolbox/assignments/week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.cpp
+++ 01-algorithmic-toolbox/assignments/week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.cpp
@@ -16,9 +16,29 @@ int get_fibonacci_last_digit_naive(int n) {
     return current % 10;
 }
 
+/*  fib n % 10 = [fib (n-1) % 10] + [fib (n-2) % 10]
+    Therefore for finding mod 10 of n th fibonacci number, we can find mod 10 of n-1 th fibonnaci and mod 10 of n-2 th
+    fibonacci. Doing it recursively we can find the answer.
+*/
+int get_fibonacci_last_digit_fast(int n) {
+    if (n <= 1) {
+        return 1;
+    }
+    int pre = 0;
+    int curr = 1;
+
+    for (int i = 2; i <= n; i++) {
+        int temp_curr = curr;
+        curr = (curr + pre) % 10;
+        pre = temp_curr;
+    }
+
+    return curr;
+}
+
 int main() {
     int n;
     std::cin >> n;
-    int c = get_fibonacci_last_digit_naive(n);
+    int c = get_fibonacci_last_digit_fast(n);
     std::cout << c << '\n';
     }
