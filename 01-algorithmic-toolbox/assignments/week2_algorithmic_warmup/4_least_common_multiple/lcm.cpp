dbda2a4e4a8919555c7aec89b296a8a58e76ff06
Ashen Umeda <ashenumeda@gmail.com>
2026-02-27 20:19:53 +0530

    Solve 01-algorithmic-toolbox week2 least_common_multiple problem

--- 01-algorithmic-toolbox/assignments/week2_algorithmic_warmup/4_least_common_multiple/lcm.cpp
+++ 01-algorithmic-toolbox/assignments/week2_algorithmic_warmup/4_least_common_multiple/lcm.cpp
@@ -8,9 +8,27 @@ long long lcm_naive(int a, int b) {
   return (long long) a * b;
 }
 
+long long gcd_Euclid(long long a, long long b) {
+  if (a > b) {
+    return gcd_Euclid(b, a);
+  }
+  if (a == 0) {
+    return b;
+  }
+  return gcd_Euclid(b % a, a);
+}
+
+/*
+  we can get the lcm of a and b by
+  (a * b) / gcd_Euclid(a, b)
+*/
+long long lcm_fast(long long a, long long b) {
+  return (a * b) / gcd_Euclid(a, b);
+}
+
 int main() {
   int a, b;
   std::cin >> a >> b;
-  std::cout << lcm_naive(a, b) << std::endl;
+  std::cout << lcm_fast(a, b) << std::endl;
   return 0;
 }
