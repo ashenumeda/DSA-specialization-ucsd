8c7c67d88b1ffb9264b1cd0d5e3ec8c28a1596a4
Ashen Umeda <ashenumeda@gmail.com>
2026-03-01 23:38:44 +0530

    Solve 01-algorithmic-toolbox week2 8_last_digit_of_the_sum_of_square_of_fibonacci_numbers

--- 01-algorithmic-toolbox/assignments/week2_algorithmic_warmup/8_last_digit_of_the_sum_of_squares_of_fibonacci_numbers/fibonacci_sum_squares.cpp
+++ 01-algorithmic-toolbox/assignments/week2_algorithmic_warmup/8_last_digit_of_the_sum_of_squares_of_fibonacci_numbers/fibonacci_sum_squares.cpp
@@ -18,8 +18,40 @@ int fibonacci_sum_squares_naive(long long n) {
     return sum % 10;
 }
 
+/*
+    We can get a formula,
+    Sn_square = (fib0)**2 + (fib1)**2 + (fib2)**2 + ... + (fibn)**2 = fibn * fibn+1
+    and,
+    (fibn * fibn+1) % 10 = (fibn % 10) * (fibn+1 % 10)
+*/
+int fibonacci_sum_squares_fast(long long n) {
+    int previous = 0;
+    int current = 1;
+
+    int pos1 = n % 60;
+    int pos2 = (n + 1) % 60;
+
+    int fibn_mod_10 = 0;
+    int fibnPlus1_mod_10 = 0;
+
+    if (pos1 <= 1) {
+        fibn_mod_10 = pos1;
+    } else {
+        for (int i = 2; i <= pos1; i++) {
+            int temp_previous = previous; //2
+            previous = current; //3
+            current = (current + temp_previous) % 10; //5
+        }
+        fibn_mod_10 = current;
+    }
+    
+    fibnPlus1_mod_10 = (current + previous) % 10;
+
+    return (fibn_mod_10 * fibnPlus1_mod_10) % 10;
+}
+
 int main() {
     long long n = 0;
     std::cin >> n;
-    std::cout << fibonacci_sum_squares_naive(n);
+    std::cout << fibonacci_sum_squares_fast(n);
 }
