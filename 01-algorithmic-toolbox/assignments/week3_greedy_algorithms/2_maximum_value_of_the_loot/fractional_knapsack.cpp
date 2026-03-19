ed8a3aff9b3e1e87c641cc726a88e7d74861626a
Ashen Umeda <ashenumeda@gmail.com>
2026-03-19 13:27:43 +0530

    Solve 01-algorithmic-toolbox week3 fractional knapsak problem

--- 01-algorithmic-toolbox/assignments/week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.cpp
+++ 01-algorithmic-toolbox/assignments/week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.cpp
@@ -5,8 +5,32 @@ using std::vector;
 
 double get_optimal_value(int capacity, vector<int> weights, vector<int> values) {
   double value = 0.0;
+  std::vector<int> used_indexes(weights.size(), 0);
 
-  // write your code here
+  while (capacity > 0) {
+    double max_value_per_unit = 0.0;
+    int index = -1;
+
+    for (int i = 0; i < weights.size(); i++) {
+      double value_per_unit = double(values[i]) / weights[i];
+
+      if (value_per_unit > max_value_per_unit && used_indexes[i]==0) {
+        max_value_per_unit = value_per_unit;
+        index = i;
+      }
+    }
+
+    if (index == -1) {return value;}
+    
+    if (weights[index] <= capacity) {
+      value += weights[index] * max_value_per_unit;      
+      capacity -= weights[index];
+    } else {
+      value += capacity * max_value_per_unit;
+      capacity -= capacity;
+    }
+    used_indexes[index] = 1;   
+  }
 
   return value;
 }
@@ -23,7 +47,8 @@ int main() {
 
   double optimal_value = get_optimal_value(capacity, weights, values);
 
-  std::cout.precision(10);
+  std::cout << std::fixed;
+  std::cout.precision(4);
   std::cout << optimal_value << std::endl;
   return 0;
 }
