fee4494d8e948a16f7f7100f73da51c6f5b4f61e
Ashen Umeda <ashenumeda@gmail.com>
2026-03-19 09:27:48 +0530

    Solve 01-algorithmic-toolbox week3 1_money_change

--- 01-algorithmic-toolbox/assignments/week3_greedy_algorithms/1_money_change/change.cpp
+++ 01-algorithmic-toolbox/assignments/week3_greedy_algorithms/1_money_change/change.cpp
@@ -2,6 +2,20 @@
 
 int get_change(int m) {
   //write your code here
+  int n = 0;
+
+  if (m == 0) {return n;}
+
+  while (m > 0) {
+    if (m >= 10) {
+      m -= 10;
+    } else if (m >= 5) {
+      m -= 5;
+    } else {
+      m -= 1;
+    }
+    n++;
+  }
   return n;
 }
 
