46b8d717bdaa150f7a434d6433936da2063ee966
Ashen Umeda <ashenumeda@gmail.com>
2026-04-01 22:52:03 +0530

    Solve 01-algorithmic-toolbox week3 6_maximum_number_of_prizes problem

--- 01-algorithmic-toolbox/assignments/week3_greedy_algorithms/6_maximum_number_of_prizes/different_summands.cpp
+++ 01-algorithmic-toolbox/assignments/week3_greedy_algorithms/6_maximum_number_of_prizes/different_summands.cpp
@@ -4,8 +4,31 @@
 using std::vector;
 
 vector<int> optimal_summands(int n) {
+  /*
+    Thsi runs in O(√n) time complexity. 
+
+    This is a greedy algorithm. And works by adding the smallest possible distinct positive integers starting from 1 until
+    the next integer would make remaining is small to be expressed as a distinct integer greater than the last added one.
+    then the remainder is appended as the final price.
+  */
   vector<int> summands;
-  //write your code here
+
+  int curr_price = 0;
+  int total_price = 0;
+
+  /*
+    Check if next price(curr_price + 1) is added, will the remaining is greater than to the next price. Otherwise we can't
+    add next price. 
+  */
+  while(n - (total_price + curr_price + 1) > curr_price + 1) { 
+    curr_price++;
+
+    summands.push_back(curr_price);
+    total_price += curr_price;
+  }
+
+  summands.push_back(n - total_price);
+
   return summands;
 }
 
